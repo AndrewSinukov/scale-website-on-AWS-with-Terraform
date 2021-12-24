e61dcc47de1c98a63766d3ef33f502fb39fdb4fe
AndrewSinukov <andrew.sinukov.it@gmail.com>
2021-12-24 14:37:50 +0200

    added user-data.tpl and attached it to launch_configuration

--- asg.tf
+++ asg.tf
@@ -31,11 +31,24 @@ resource "aws_security_group" "dfsc_asg_sg" {
   }
 }
 
+data "template_file" "userdata_template" {
+  template = file("user-data.tpl")
+  vars = {
+    db_host      = aws_db_instance.dfsc_db.address
+    db_username  = aws_db_instance.dfsc_db.username
+    db_password  = aws_db_instance.dfsc_db.password
+    db_name      = aws_db_instance.dfsc_db.name
+    cache_host   = aws_elasticache_replication_group.dfsc_elasticache.primary_endpoint_address
+    efs_endpoint = aws_efs_file_system.dfsc_efs.dns_name
+  }
+}
+
 resource "aws_launch_configuration" "dfsc_launch_config" {
   name_prefix     = "DFSC Launch Configuration"
   image_id        = "ami-of2ed58082cb08a4d"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.dfsc_asg_sg.id]
+  user_data       = data.template_file.userdata_template.rendered
   lifecycle {
     create_before_destroy = true
   }
